9c946bf2f41f630fd939abc5c3f778e7a2fb6740
Ben Kreeger <ben@kree.gr>
2013-01-02 09:25:31 -0600

    Initial commit.

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+maintainer        'Ben Kreeger'
+maintainer_email  'ben@kree.gr'
+license           'MIT'
+description       'Personal settings and cookbooks.'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '1.0'
+supports          'mac_os_x'
+
+depends           'pivotal_workstation'
---
+++ readme.markdown
@@ -0,0 +1,5 @@
+# kreeger_workstation
+
+Shamelessly inspired by [roderik_workstation][rw]. Bootstrapping and personal Chef recipes for setting up my development environment.
+
+[rw]:     https://github.com/roderik/roderik_workstation
